b8ec8c8126ecb0a1c1cba798a6bc46c3c96a23e9
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-25 23:30:44 +0300

    pseudo

--- spinwords.js
+++ spinwords.js
@@ -2,3 +2,5 @@
 
 //The parameters we input into our function are sentences made up of strings and we do not expect either integers or empty parameters
 //We are going to return the input sentences with  their words that have more than five characters reversed and words with less than 5 characters as they were
+// We split the sentence into individual array elements, we loop through the array and for elements of length greater/equal to 5 are  split, reversed then joined and store it in a new variable
+// We then return the new variable
