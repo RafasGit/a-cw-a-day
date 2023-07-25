6cc5a9061cc0d1aff80e777cef8a63e200bde4e6
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-25 23:23:36 +0300

    returns

--- spinwords.js
+++ spinwords.js
@@ -1,3 +1,4 @@
 // We input a sentence and if the length of the words is greater than 5 strings, we return the reversed version of the word
 
 //The parameters we input into our function are sentences made up of strings and we do not expect either integers or empty parameters
+//We are going to return the input sentences with  their words that have more than five characters reversed and words with less than 5 characters as they were
