1144c1b25b6f2e4cbcfc0dbef431b60d8a084982
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-26 18:01:10 +0300

    examples

--- whitespace.js
+++ whitespace.js
@@ -2,4 +2,11 @@
 // We are expecting the parameters in string or number format with spacing between them
 //We return the strings without any spacing between individual string items
 // example test cases would be ghd djsks ajs -> ghddjsksajs
+
 // We split the input strings with a given criteria into array elements, we then join the array elements into a single string with whitespace removed  
+
+function noSpace(x){
+    return x.split(' ').join('')
+    }
+
+    
