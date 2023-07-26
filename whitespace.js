3a9218f29c6aaf78326a9042fca46cdeac462102
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-26 17:57:23 +0300

    examples

--- whitespace.js
+++ whitespace.js
@@ -1,3 +1,4 @@
 // We are tasked with accepting an input and remove the white space between strings or sentences
 // We are expecting the parameters in string or number format with spacing between them
 //We return the strings without any spacing between individual string items
+// example test cases would be ghd djsks ajs -> ghddjsksajs
