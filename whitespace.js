b1d74f60be6552ca42bed18e1dd7bfb3321a8f7d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-26 17:53:54 +0300

    parameters

--- whitespace.js
+++ whitespace.js
@@ -1,1 +1,2 @@
 // We are tasked with accepting an input and remove the white space between strings or sentences
+// We are expecting the parameters in stringor number format with spacing between them
