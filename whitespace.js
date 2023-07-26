630a9cbad04ea0de111bb04c1a2abc4ca6f3f620
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-26 17:55:44 +0300

    returns

--- whitespace.js
+++ whitespace.js
@@ -1,2 +1,3 @@
 // We are tasked with accepting an input and remove the white space between strings or sentences
-// We are expecting the parameters in stringor number format with spacing between them
+// We are expecting the parameters in string or number format with spacing between them
+//We return the strings without any spacing between individual string items
