24436572f2ffb3b2e35aba50f9019d832085845e
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-27 16:43:17 +0300

    solution

--- strrpt.js
+++ strrpt.js
@@ -3,3 +3,11 @@
 // We are expected to return the input string for the number of times the integer demands
 // An example input would be 3, h -> 'hhh', 2, jon -> 'jonjonjon'
 //We will declare our variable to store the repeated string, we then run a loop equal to iterations given by input number, for each time adding the string
+
+function repeatStr (n, s) {
+    let conc = ''
+    for(let i=0; i<n; i++){
+         conc +=`${s}`;
+    }
+    return conc;
+  }
