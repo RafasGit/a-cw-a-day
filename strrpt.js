fb0c34752c189f3e87477ff3fd78dc22eb7775a4
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-27 16:40:05 +0300

    examples

--- strrpt.js
+++ strrpt.js
@@ -1,3 +1,4 @@
 // We are to take in a single string and integer and return the input string for the number of times matching the input integer
 // Our expected parameters are a string input and number
 // We are expected to return the input string for the number of times the integer demands
+// An example input would be 3, h -> 'hhh', 2, jon -> 'jonjonjon'
