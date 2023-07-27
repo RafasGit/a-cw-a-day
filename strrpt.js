191ac1475a2e0a054ddc9f1cc9e600d2b5d2f0d7
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-27 16:35:21 +0300

    parameters

---
+++ strrpt.js
@@ -0,0 +1,2 @@
+// We are to take in a single string and integer and return the input string for the number of times matching the input integer
+// Our expected parameters are a string input and number
