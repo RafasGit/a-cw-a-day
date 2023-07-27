badbdbdb42c3334d51133543ee210f7fe0639bda
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-27 16:37:11 +0300

    returns

--- strrpt.js
+++ strrpt.js
@@ -1,2 +1,3 @@
 // We are to take in a single string and integer and return the input string for the number of times matching the input integer
 // Our expected parameters are a string input and number
+// We are expected to return the input string for the number of times the integer demands
