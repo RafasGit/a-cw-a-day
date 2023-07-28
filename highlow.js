94759373b91caf06d94809fab4332d6a66483cf1
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-28 13:43:35 +0300

    examples

--- highlow.js
+++ highlow.js
@@ -1,3 +1,4 @@
 // We are required to accept a string of space separated numbers, no alphabet and return the largest and smallest value
 // We are going to pass on a string of numbers as our parameters, no text, negative numbers can be included
-// We are going to return the highest and lowest number from the string of numbers passed 
+// We are going to return the highest and lowest number inside one string from the string of numbers passed 
+// For an input of '5 4 6 2 1' we return '5 1'
