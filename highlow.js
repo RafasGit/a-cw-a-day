3982cfa0b7d487cd9068d801f866b7590d221b70
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-28 13:39:33 +0300

    parameters

--- highlow.js
+++ highlow.js
@@ -1,1 +1,2 @@
 // We are required to accept a string of space separated numbers, no alphabet and return the largest and smallest value
+// We are going to pass on a string of numbers as our parameters, no text, negative numbers can be included
