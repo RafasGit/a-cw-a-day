728f16c4b35ba86c89147b6352b288317ffe2e3d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-28 13:47:51 +0300

    pseudo

--- highlow.js
+++ highlow.js
@@ -2,3 +2,5 @@
 // We are going to pass on a string of numbers as our parameters, no text, negative numbers can be included
 // We are going to return the highest and lowest number inside one string from the string of numbers passed 
 // For an input of '5 4 6 2 1' we return '5 1'
+// We initialize our function with our parameter we then split the string elements into individual array elements.
+//This allows us to use native array math.max and math.min  along with array destructuring to template string our largest and smallest number
