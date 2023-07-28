fdb7777ba40fc67aa9feb8554601a9a7329e0069
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-28 13:41:20 +0300

    return

--- highlow.js
+++ highlow.js
@@ -1,2 +1,3 @@
 // We are required to accept a string of space separated numbers, no alphabet and return the largest and smallest value
 // We are going to pass on a string of numbers as our parameters, no text, negative numbers can be included
+// We are going to return the highest and lowest number from the string of numbers passed 
