95e4de153c7db378c4a729fd3420acaefcfa0bc8
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-29 02:09:09 +0300

    parameters

--- createNum.js
+++ createNum.js
@@ -1,2 +1,2 @@
-// We are to input an array of numbers and create it into a string phone number format 
+// We are to input an array of numbers and create it into a string phone number format style 
 // We expect parameters  in the form of an array of 10 digits, no text no string
