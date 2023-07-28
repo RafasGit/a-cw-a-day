ba900b046bb82d508bb43953e774deb644125f6b
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-29 02:16:18 +0300

    pseudo

--- createNum.js
+++ createNum.js
@@ -2,3 +2,4 @@
 // We expect parameters  in the form of an array of 10 digits, no text no string
 // we are returning a phone number split into its country code and the remaining numbers split into 3 and 4 part bits
 //  An example test case would be 0111799390 -> '(011) 179-9390'
+// We join the nums into one big string and return the first three nums, then the next 3 add our hyphen and add the final substring
