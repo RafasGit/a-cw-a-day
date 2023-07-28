f68f643aa5a5f0cd21d8ffb98554851a20403b50
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-29 02:10:59 +0300

    returns

--- createNum.js
+++ createNum.js
@@ -1,2 +1,3 @@
 // We are to input an array of numbers and create it into a string phone number format style 
 // We expect parameters  in the form of an array of 10 digits, no text no string
+// we are returning a phone number split into its country code and the remaining numbers split into 3 and 4 part bits
