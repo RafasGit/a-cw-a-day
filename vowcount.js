d71bf1b1419c4adcbaf63e69b1a7371e4e342c2f
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-30 22:47:52 +0300

    pseudo

--- vowcount.js
+++ vowcount.js
@@ -2,3 +2,4 @@
 // We take in a parameter of an input string with only lower case letters and/or spaces and no numbers
 // We are returning the number of vowels in the input string
 //An input of the string 'achieve' will return 4, 'Mastery' -> 2, 'celebrate' -> 4
+// We take the input string, split it into array elements, create different vowel array, filter original array into new array including alphabet similar to vowel array
