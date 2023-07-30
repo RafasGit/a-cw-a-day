63969b26dc0f3a9e084a08fd1ab745113f60019a
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-30 22:26:03 +0300

    return

--- vowcount.js
+++ vowcount.js
@@ -1,2 +1,3 @@
 // We are to receive a string input and return the number of vowels in the string.
 // We take in a parameter of an input string with only lower case letters and/or spaces and no numbers
+// We are returning the number of vowels in the input string
