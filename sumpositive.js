b4273b06c3ec7d58ade30ac48782058fb85fb4cc
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-31 23:30:28 +0300

    examples

--- sumpositive.js
+++ sumpositive.js
@@ -2,3 +2,4 @@
 // Our parameters may inlcude an empty array or an array of numbers without negative numbers
 // We return the sum of only the positive numbers 
 // Example test cases; [1, 2, -3, -6, 9] => 12 [3, -4, 6, -7, 9, 10] => 28  [5, -4, 9, -10, 8] => 22
+// We filter the array for nums greater or equal to 0 and then reduce the new array and return the sum of this numbers
