154a3744d29bcda225ca18a304706deabd0af157
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-31 23:27:22 +0300

    returns

--- sumpositive.js
+++ sumpositive.js
@@ -1,3 +1,4 @@
 // We accept an input of an array of numbers and return the sum of only positive numbers
 // Our parameters may inlcude an empty array or an array of numbers without negative numbers
 // We return the sum of only the positive numbers 
+// Example test cases; [1, 2, -3, -6, 9] => 12 [3, -4, 6, -7, 9, 10] => 28  [5, -4, 9, -10, 8] => 22
