3b7e065bc02a1b229fb679f6e5262cecc266963f
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-31 23:31:31 +0300

    solution

--- sumpositive.js
+++ sumpositive.js
@@ -3,3 +3,6 @@
 // We return the sum of only the positive numbers 
 // Example test cases; [1, 2, -3, -6, 9] => 12 [3, -4, 6, -7, 9, 10] => 28  [5, -4, 9, -10, 8] => 22
 // We filter the array for nums greater or equal to 0 and then reduce the new array and return the sum of this numbers
+function positiveSum(arr) {
+    return arr.filter(num => num >= 0).reduce((a,b) => a+b, 0)
+  }
