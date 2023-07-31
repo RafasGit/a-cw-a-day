f52c7f558f17fd27666942d4cde355537b7e6036
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-31 23:21:13 +0300

    problem statement

--- sumpositive.js
+++ sumpositive.js
@@ -1,1 +1,2 @@
 // We accept an input of an array of numbers and return the sum of only positive numbers
+// Our parameters may inlcude an empty array or an array of numbers without negative numbers
