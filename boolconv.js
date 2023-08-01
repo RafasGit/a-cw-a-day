a24896271dd80708c7a3f4bd11bbf869329bd28d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-01 23:20:42 +0300

    solution

--- boolconv.js
+++ boolconv.js
@@ -2,4 +2,6 @@
 // We take in parameters of boolean format true and false 
 // we are to return string values of 'Yes' for boolean value true and 'No' for boolean value false
 // (false) => 'No' (true) => 'Yes'
-// We create a function and accept our  boolean input, input is put in a conditional and return required string  
+// We create a function and accept our  boolean input, input is put in a conditional and return required string
+
+   const boolToWord = bool => bool === true ? 'Yes' : 'No'
