17e1fd6dcf4fede71b946a961a8122b7247c3819
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-01 23:19:02 +0300

    pseudo

--- boolconv.js
+++ boolconv.js
@@ -2,3 +2,4 @@
 // We take in parameters of boolean format true and false 
 // we are to return string values of 'Yes' for boolean value true and 'No' for boolean value false
 // (false) => 'No' (true) => 'Yes'
+// We create a function and accept our  boolean input, input is put in a conditional and return required string  
