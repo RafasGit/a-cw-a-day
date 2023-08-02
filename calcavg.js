8f4b9d46fae31cbfc28af91ee1d4ff7581107a40
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-02 23:25:16 +0300

    returns

--- calcavg.js
+++ calcavg.js
@@ -1,3 +1,4 @@
  
  // We are to intake a list of numbers and return the average of the input numbers
  // Our expected parameters are is an array of integers with no strings. Empty arrays should return 0
+ // We are returning the average of the array of numbers and 0 for empty arrays
