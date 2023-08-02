acbb7022651025c6d7c8138dea6e26d91f3e57de
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-02 23:27:46 +0300

    examples

--- calcavg.js
+++ calcavg.js
@@ -2,3 +2,4 @@
  // We are to intake a list of numbers and return the average of the input numbers
  // Our expected parameters are is an array of integers with no strings. Empty arrays should return 0
  // We are returning the average of the array of numbers and 0 for empty arrays
+ // [5, 4, 3, 2] => 3.5 [2, 4, 3, 1. 5] => 3, [] => 0
