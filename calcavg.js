1aaaf99b0ebc8483a2ef7322ec4174772aa3fd40
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-02 23:32:44 +0300

    solution

--- calcavg.js
+++ calcavg.js
@@ -5,3 +5,20 @@
  // [5, 4, 3, 2] => 3.5 [2, 4, 3, 1. 5] => 3, [] => 0
  // We accept the input of array of numbers, confirm the array is not empty, if epty return zero, else loop through the array and add each num to a new variable
  // Divide the value of the added numbers by length of input array
+  
+
+ const findAverage = array =>{
+    let sum = 0
+    if(array.length != 0 ){
+      array.forEach((num,i) =>{
+    sum += array[i]
+      })
+       let average = sum / array.length
+       return average;
+      }
+    else 
+      {
+        return 0;
+      }
+     
+      }
