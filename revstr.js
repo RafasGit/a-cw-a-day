a405f406849a066d613375534c6229ec662f3772
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-03 22:42:16 +0300

    pseudo

--- revstr.js
+++ revstr.js
@@ -2,3 +2,4 @@
 // We accept the parameter of a string without any number, our string should also not be empty
 // Our expected return should be the string reversed
 // test cases; 'world' => 'dlrow',  'dog' => 'god'  'code' => 'edoc'
+// We take in the string input, we split it into array elements, allowing us to use the native array method reverse and finally join the elements into our reversed string and return the result
