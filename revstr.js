f5fa1de27c1d43437ce91f667eb1a07cef9b301f
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-03 22:37:43 +0300

    return

--- revstr.js
+++ revstr.js
@@ -1,2 +1,3 @@
 // We take in a string and return the reversed output
 // We accept the parameter of a string without any number, our string should also not be empty
+// We our expected return should be the string reversed
