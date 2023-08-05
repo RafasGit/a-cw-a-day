be92dda680d7b6f6ab60183794e9fca367e99a5b
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-05 23:33:33 +0300

    parameters

--- numtoString.js
+++ numtoString.js
@@ -1,2 +1,3 @@
 // We accept the input of an integer and return it converted into a string
 // We take in the parameters of integer datatype, no strings, no empty parameters
+// We return an output of the input numbers converted into string format
