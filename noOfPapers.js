236345a37d9180f5f3b220d8df39469cadbeb15d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-06 19:42:49 +0300

    returns

--- noOfPapers.js
+++ noOfPapers.js
@@ -1,2 +1,3 @@
 // We accept two inputs, one for the number of students and one for the number of pages per student required and calculate the total pages used
 // We set two parameters, one for number of students and second one for pages per student. If either parameter is set to value of zero, we return 0
+// We return the value of number of total copies required and if no copies required, we return 0
