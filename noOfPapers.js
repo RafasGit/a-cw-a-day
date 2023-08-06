6c031aeb08b5950069c5dfa4271016a0e9421d65
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-06 20:03:13 +0300

    We accept two inputs, one for the number of students and one for the number of pages per student required and calculate the total pages used

--- noOfPapers.js
+++ noOfPapers.js
@@ -1,7 +1,7 @@
 // We accept two inputs, one for the number of students and one for the number of pages per student required and calculate the total pages used
 // We set two parameters, one for number of students and second one for pages per student. If either parameter is set to value of zero or is less than zero, we return 0
 // We return the value of number of total copies required and if no copies required, we return 0
-//  Our testcases include; n = 6, m = 4 : 24, n = 7, m = 0 : 0, n = 5, m = 3 : 15
+//  Our testcases include; n = 6, m = 4 : 24, n = 7, m = 0 : 0, n = -5, m = 6 : 0
 // We initialize our function and declare our two parameters; n(no. of students) and m(no. of pages per student), check if both values are greater than 0 and return product of n and m;
 
 
