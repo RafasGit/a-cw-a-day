f6fd48d29a33a95d2bf6919b4a29d11d6a4eb230
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-06 19:50:33 +0300

    examples

--- noOfPapers.js
+++ noOfPapers.js
@@ -1,3 +1,4 @@
 // We accept two inputs, one for the number of students and one for the number of pages per student required and calculate the total pages used
 // We set two parameters, one for number of students and second one for pages per student. If either parameter is set to value of zero, we return 0
 // We return the value of number of total copies required and if no copies required, we return 0
+//  Our testcases include; n = 6, m = 4 : 24, n = 7, m = 0 : 0, n = 5, m = 3 : 15
