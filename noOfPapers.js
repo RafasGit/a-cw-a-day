b1bb4a26257950962abd085043024b11d549ebb4
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-06 19:52:48 +0300

    pseudo

--- noOfPapers.js
+++ noOfPapers.js
@@ -2,3 +2,4 @@
 // We set two parameters, one for number of students and second one for pages per student. If either parameter is set to value of zero, we return 0
 // We return the value of number of total copies required and if no copies required, we return 0
 //  Our testcases include; n = 6, m = 4 : 24, n = 7, m = 0 : 0, n = 5, m = 3 : 15
+// We initialize our function and declare our two parameters; n(no. of students) and m(no. of pages per student) and return product of n and m;
