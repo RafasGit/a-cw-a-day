c05c298113aca578491f7266170c117e63c99b05
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-07 18:56:36 +0300

    examples

--- pointsCount.js
+++ pointsCount.js
@@ -1,3 +1,4 @@
 // We accept the input of the results of our games and return the number of points has scored.
 // We initialize our function with string parameters in the format 'x;y' with x being our teams score and y our opponents score
 // We return the number of points our team scores depending on the number of wins, losses and draws
+// Examples; ['3:2', '2:2', '2:3'] => 4, ['2:2', '3;1', '3:0'] => 7, ['1:1', '2:3', '3:4'] => 1
