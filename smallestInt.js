2b2a4db8e416cb29d0c0c4b5bc62322c8ae68bf1
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-08 00:24:29 +0300

    returns

--- smallestInt.js
+++ smallestInt.js
@@ -1,2 +1,3 @@
 // We are to input an array of numbers and return the smallest number 
 // We accept the parameter of an array of integers and the array should only contain integers and shouldnt be an empty array
+// We return the smallest integer in our array
