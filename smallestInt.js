392c95de87b2501fdead511b93a6f02726c93815
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-08 00:33:22 +0300

    pseudo

--- smallestInt.js
+++ smallestInt.js
@@ -2,3 +2,4 @@
 // We accept the parameter of an array of integers and the array should only contain integers and shouldnt be an empty array
 // We return the smallest integer in our array
 // Test cases [1, 32, 43, 4] => 1, [2, 4, 5, 6] => 2, [4, 6, 7, 45] => 4
+// We initialize our function and using our array if numbers parameter, we sort our array in ascending order and return the first element of our array
