fc055f426d91ac7c0958a6b3de727c25982bcac5
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-07 19:00:14 +0300

    pseudo

--- pointsCount.js
+++ pointsCount.js
@@ -2,3 +2,4 @@
 // We initialize our function with string parameters in the format 'x;y' with x being our teams score and y our opponents score
 // We return the number of points our team scores depending on the number of wins, losses and draws
 // Examples; ['3:2', '2:2', '2:3'] => 4, ['2:2', '3;1', '3:0'] => 7, ['1:1', '2:3', '3:4'] => 1
+// We declare our points variable, split the results using ':' as our criteria, we set our conditional to check if our result is greater than the opponents and award our team 3 points
