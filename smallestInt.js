71dbb2f050581ae5b937836db2f556bcca7ab59d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-08 00:26:51 +0300

    examples

--- smallestInt.js
+++ smallestInt.js
@@ -1,3 +1,4 @@
 // We are to input an array of numbers and return the smallest number 
 // We accept the parameter of an array of integers and the array should only contain integers and shouldnt be an empty array
 // We return the smallest integer in our array
+// Test cases [1, 32, 43, 4] => 1, [2, 4, 5, 6] => 2, [4, 6, 7, 45] => 4
