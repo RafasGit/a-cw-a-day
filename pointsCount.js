cf73fa1c0f6f7e6187a745483f75b1160a092f1d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-07 18:52:33 +0300

    Parameters

--- pointsCount.js
+++ pointsCount.js
@@ -1,2 +1,3 @@
 // We accept the input of the results of our games and return the number of points has scored.
 // We initialize our function with string parameters in the format 'x;y' with x being our teams score and y our opponents score
+// We return the number of points our team scores depending on the number of wins, losses and draws
