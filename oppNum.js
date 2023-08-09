5276f4592cd35763a99681e73c7cdc1cc4424c1f
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-09 23:22:48 +0300

    solution file

--- oppNum.js
+++ oppNum.js
@@ -3,3 +3,7 @@
 // We return the opposite value of our input number
 // Examples; 1: -1, 34: -34, 50: -50
 // We accept the input integer, and return the product of the number with -1 
+function opposite(number) {
+  return - number
+}
+
