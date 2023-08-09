e9d1873070ce7539dd67cfa778c9e19f82ae019a
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-09 23:24:40 +0300

    test cases

--- oppNum.js
+++ oppNum.js
@@ -1,9 +1,10 @@
 // We are given an integer and are to return its opposite
 // We accept the parameters of integer form and no strings or empty parameters
 // We return the opposite value of our input number
-// Examples; 1: -1, 34: -34, 50: -50
+// Examples; 1: -1, -34: 34, 50: -50
 // We accept the input integer, and return the product of the number with -1 
 function opposite(number) {
   return - number
 }
 
+opposite(1) // -1
