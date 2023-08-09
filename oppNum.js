ac5563da34edf4b70b74a86743472f9f79ba2bcb
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-09 23:18:04 +0300

    returns

--- oppNum.js
+++ oppNum.js
@@ -1,2 +1,3 @@
 // We are given an integer and are to return its opposite
 // We accept the parameters of integer form and no strings or empty parameters
+// We return the opposite value of our input number
