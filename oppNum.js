d0b67d085252a18034974994ebac7164c733ea57
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-09 23:19:27 +0300

    examples

--- oppNum.js
+++ oppNum.js
@@ -1,3 +1,4 @@
 // We are given an integer and are to return its opposite
 // We accept the parameters of integer form and no strings or empty parameters
 // We return the opposite value of our input number
+// Examples; 1: -1, 34: -34, 50: -50
