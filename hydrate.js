2093f3929def8a654d37efa882ae1a77e1e2afb1
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-10 22:21:46 +0300

    pseudo

--- hydrate.js
+++ hydrate.js
@@ -2,3 +2,4 @@
 // We create the parameter 'time' which will take in the number of hours our cyclist will be actively cycling in integer format, no strings will be passed and empty arguments .
 // We are going to return the total number of litres our cyclist will need rounded to the smallest value
 // A few test cases include; time = 3; --> 1.5, time = 2.2; --> 1, time = 0; --> 0
+// We accept the given input, multiply the value by 0.5 and use math method floor which rounds our product to the smallest value and return the result
