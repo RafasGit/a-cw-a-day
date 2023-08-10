f0516b120964a518e9e9272b6f1e841a511669f2
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-10 22:20:10 +0300

    examples

--- hydrate.js
+++ hydrate.js
@@ -1,3 +1,4 @@
-// We are to create a function that tells our cyclist the number of litres they need to drink for given number of hours
-// We create the parameter 'time' which will take in the number of hours our cyclist will be actively cycling
-// We are going to return the total number of litres our cyclist will need
+// We are to create a function that tells our cyclist the number of litres they need to drink for given number of hours given that he consumes 0.5 litres per hour
+// We create the parameter 'time' which will take in the number of hours our cyclist will be actively cycling in integer format, no strings will be passed and empty arguments .
+// We are going to return the total number of litres our cyclist will need rounded to the smallest value
+// A few test cases include; time = 3; --> 1.5, time = 2.2; --> 1, time = 0; --> 0
