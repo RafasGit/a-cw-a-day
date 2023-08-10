c77726444e8cd1f7721840e9061d374732b3d8b8
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-10 22:11:12 +0300

    parameters

--- hydrate.js
+++ hydrate.js
@@ -1,1 +1,2 @@
 // We are to create a function that tells our cyclist the number of litres they need to drink for given number of hours
+// We create the parameter 'time' which will take in the number of hours our cyclist will be actively cycling
