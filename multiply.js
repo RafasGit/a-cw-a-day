4e50b8daccd110294f5896c5e1835cabf68a441d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-11 01:16:30 +0300

    examples

--- multiply.js
+++ multiply.js
@@ -1,3 +1,4 @@
 // We create a function to multiply the number 5 raised to the power of number of digits of given number by the given number
 // We define a parameter that takes in the number to be multiplied by 5 with 
 // We return the result of the multiplication
+// Examples multiply(3) --> 15, multiply(10) --> 250, multiply(-2) --> -10
