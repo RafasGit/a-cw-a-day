55e9c9ee755b7e881098c0f251909e696f1b516a
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-10 22:12:06 +0300

    returns

--- hydrate.js
+++ hydrate.js
@@ -1,2 +1,3 @@
 // We are to create a function that tells our cyclist the number of litres they need to drink for given number of hours
 // We create the parameter 'time' which will take in the number of hours our cyclist will be actively cycling
+// We are going to return the total number of litres our cyclist will need
