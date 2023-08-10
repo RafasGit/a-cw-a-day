ef0351b348d8d5c1ecda179afce163f12828ef48
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-11 01:14:12 +0300

    parameters

--- multiply.js
+++ multiply.js
@@ -1,1 +1,2 @@
 // We create a function to multiply the number 5 raised to the power of number of digits of given number by the given number
+// We define a parameter that takes in the number to be multiplied by 5 with 
