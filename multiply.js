ac146af8c5112128aab2b641395687854d8ba691
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-11 01:14:50 +0300

    returns

--- multiply.js
+++ multiply.js
@@ -1,2 +1,3 @@
 // We create a function to multiply the number 5 raised to the power of number of digits of given number by the given number
 // We define a parameter that takes in the number to be multiplied by 5 with 
+// We return the result of the multiplication
