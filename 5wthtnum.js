2d5c0d8288ce1864d33e47d6a122cbcc36cd57ef
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-12 23:44:55 +0300

    examples

--- 5wthtnum.js
+++ 5wthtnum.js
@@ -1,3 +1,4 @@
 // We are to return 5 without using any integer or any mathematical operator
 // We declare the parameters of a string of five letter and we will get its length
 // We return the integer 5
+// unusualFive(color) => 5, unusualFive(tests) => 5, unusualFive(emacs) => 5
