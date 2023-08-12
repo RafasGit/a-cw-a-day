8e7ac52fa290981252e3956aa6b0ba8989832f31
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-13 01:05:16 +0300

    PSeudo

--- evenodd.js
+++ evenodd.js
@@ -2,3 +2,4 @@
 // We declare parameters of integer datatype, of either negative or positive format.
 // Our returns are either string type, 'Even' or 'Odd'
 // evenOrOdd(5) => Odd, evenOrOdd(6) => Even,  evenOrOdd(-1) => Odd
+// We accept the number, we declare a function with a conditional to check if input is divisible by 0 to return 'Even', else return 'False'
