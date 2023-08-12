41595297f9ad800a4a1124a2fc2640994c00ee9c
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-12 23:35:41 +0300

    return

--- 5wthtnum.js
+++ 5wthtnum.js
@@ -1,2 +1,3 @@
 // We are to return 5 without using any integer or any mathematical operator
 // We declare the parameters of a string of five letter and we will get its length
+// We return the integer 5
