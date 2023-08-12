3816e38acc5b0b9fed6f64f42c14f64a78d93ca3
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-13 00:57:54 +0300

    returns

--- evenodd.js
+++ evenodd.js
@@ -1,2 +1,3 @@
 // We are to create a function to determine if input number is even or odd
 // We declare parameters of integer datatype, of either negative or positive format.
+// Our returns are either string type, 'Even' or 'Odd'
