1c49a762b6edbee390d80eaff293f5e6de2367df
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-12 23:50:57 +0300

    solution

--- 5wthtnum.js
+++ 5wthtnum.js
@@ -3,3 +3,10 @@
 // We return the integer 5
 // unusualFive(color) => 5, unusualFive(tests) => 5, unusualFive(emacs) => 5
 // We will input an array with five elements, find the length of the array and return the value
+
+function unusualFive() {
+    let arr = ['five', 'four', 'three', 'two', 'one']
+    
+     return arr.length
+    
+  }
