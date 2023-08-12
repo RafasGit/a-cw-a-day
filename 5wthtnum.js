719e5a70ec19097b18d550fc2889082c976171be
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-12 23:50:07 +0300

    pseudo

--- 5wthtnum.js
+++ 5wthtnum.js
@@ -1,4 +1,5 @@
 // We are to return 5 without using any integer or any mathematical operator
-// We declare the parameters of a string of five letter and we will get its length
+// We declare the parameters of an array of five elements and we will get its length
 // We return the integer 5
 // unusualFive(color) => 5, unusualFive(tests) => 5, unusualFive(emacs) => 5
+// We will input an array with five elements, find the length of the array and return the value
