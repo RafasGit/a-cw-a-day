fa6b18235019df3cbdc8ffd65e2f7ebe162f2bc3
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-14 23:26:56 +0300

    solution

--- countdupl.js
+++ countdupl.js
@@ -3,3 +3,11 @@
 // We return the count of repeated string numbers or alphabets
 // Test cases duplicateCount("abcde"), 0); duplicateCount("aabbcde"), 2); (duplicateCount("abbcde"), 1);
 // We accept our input, split it into individual array elements, use filter array method to check if first position of an element is equal to first one else it is repeated and return filtered array length
+function duplicateCount(text){
+    //...
+    return text.toLowerCase().split('').filter(( el, i, arr) => {
+     return arr.indexOf(el) !== i && arr.lastIndexOf(el) === i;                               
+  }).length;
+   }                                         
+                                               
+  
