82383b27f5385bb46b730925211cc1ef94f3c3a2
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-14 23:27:09 +0300

    solution

--- countdupl.js
+++ countdupl.js
@@ -11,3 +11,8 @@ function duplicateCount(text){
    }                                         
                                                
   
+  // function duplicateCount(text){
+  //   return text.toLowerCase().split('').filter(function(val, i, arr){
+  //     return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  //   }).length;
+  // }
