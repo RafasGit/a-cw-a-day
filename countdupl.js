8444f78363ff98ea6f68730884b10329adef0d05
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-14 23:23:55 +0300

    examples

--- countdupl.js
+++ countdupl.js
@@ -1,3 +1,5 @@
 // Create a function that returns a function that returns the number of alphabetic characters and or numeric digits that have been repeated irregardless of case sensitivity
 // we declare parameter to take in input text of string datatype
 // We return the count of repeated string numbers or alphabets
+// Test cases duplicateCount("abcde"), 0); duplicateCount("aabbcde"), 2); (duplicateCount("abbcde"), 1);
+   
