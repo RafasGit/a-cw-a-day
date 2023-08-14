71258d7ca42f8266d47d31c1aebba12b5045b937
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-14 23:21:37 +0300

    returns

--- countdupl.js
+++ countdupl.js
@@ -1,2 +1,3 @@
 // Create a function that returns a function that returns the number of alphabetic characters and or numeric digits that have been repeated irregardless of case sensitivity
 // we declare parameter to take in input text of string datatype
+// We return the count of repeated string numbers or alphabets
