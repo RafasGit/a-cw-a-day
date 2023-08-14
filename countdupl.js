90995df0dc43609aa40f0a4605b3e63d17ce52d6
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-14 23:26:13 +0300

    pseudo

--- countdupl.js
+++ countdupl.js
@@ -2,4 +2,4 @@
 // we declare parameter to take in input text of string datatype
 // We return the count of repeated string numbers or alphabets
 // Test cases duplicateCount("abcde"), 0); duplicateCount("aabbcde"), 2); (duplicateCount("abbcde"), 1);
-   
+// We accept our input, split it into individual array elements, use filter array method to check if first position of an element is equal to first one else it is repeated and return filtered array length
