420ec15944e733bb3fe4e1f2b517bfddbd085c5a
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-15 07:09:04 +0300

    tests

--- duplenc.js
+++ duplenc.js
@@ -1,7 +1,7 @@
 // We are to write a function that converts characters in a string into new string either '(' if converted string appears once or ')' if string appears twice
 // We declare a single parameter that will take in input string, no integer or empty string inputs are expected, case should be ignored
 // We return the string '(' for characters that appear once and ')' for characters that are repeated
-//  Test cases; duplicateEncode('tommmy') --> '(())(', duplicateEncode('Musa') --> '((((' duplicateEncode('exceed') --> ')(())('
+//  Test cases; duplicateEncode('tommmy') --> '(())(', duplicateEncode('Musa') --> '(((('; duplicateEncode('exceed') --> ')(())('
 // We accept the input string, split the string into individual array elements, map through the array, with a conditional confirming if initial positioning of an element is similar to the equal position
 // If element is in the same position, it is not duplicated thus we return '(' else we return ')' for each instance of the repeated character. We then join the returned array elements into a single string.
 
@@ -16,3 +16,4 @@ function duplicateEncode(word){
 }
 
 console.log(duplicateEncode('tommmy')) // --> '(())('
+console.log(duplicateEncode('Musa'))  // --> '(((('
