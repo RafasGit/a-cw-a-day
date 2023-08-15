b8e7657de6d2444a72fdd1cfcc222353d30c800d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-15 07:03:58 +0300

    Pseudo

--- duplenc.js
+++ duplenc.js
@@ -2,4 +2,5 @@
 // We declare a single parameter that will take in input string, no integer or empty string inputs are expected, case should be ignored
 // We return the string '(' for characters that appear once and ')' for characters that are repeated
 //  Test cases; duplicateEncode('tommmy') --> '(())(', duplicateEncode('Musa') --> '((((' duplicateEncode('exceed') --> ')(())('
-// We accept the input string, split the string into individual array elements, map through the array, confirming if initial positioning of an element is similar to the equal position
+// We accept the input string, split the string into individual array elements, map through the array, with a conditional confirming if initial positioning of an element is similar to the equal position
+// If element is in the same position, it is not duplicated thus we return '(' else we return ')' for each instance of the repeated character.
