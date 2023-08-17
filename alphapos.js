b931a7ec2536206e6fce4a55f1ed3ba396f86a06
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-17 10:38:26 +0300

    draft

--- alphapos.js
+++ alphapos.js
@@ -1,4 +1,7 @@
-// Problem state
+// Problem state; we will replace each letter with its position in the alphabet and return the output
+
+
+
 // Define parameters
 //Give expected return if any
 // Give examples/tests
