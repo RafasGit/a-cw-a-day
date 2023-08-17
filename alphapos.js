bfd807ca05b688b8356ff764ffe48f8c2b3c4a96
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-17 11:18:20 +0300

    Problem state; we will replace each letter with its position in the alphabet and return the output

--- alphapos.js
+++ alphapos.js
@@ -22,7 +22,7 @@ function alphabetPosition(text) {
     return newAlpha.join(' ');
   }
 
-// Use test cases / log on console
+// Use test cases / log on console.
 console.log(alphabetPosition('Abcde')) //  '1 2 3 4 5'
 console.log(alphabetPosition('fghij')) // '6 7 8 9 10'
 console.log(alphabetPosition('klmno')) // '11 12 13 14 15'
