4647cf40dd8e2f2c2094abebff2ef0811783b56a
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-17 10:44:17 +0300

    parameters

--- alphapos.js
+++ alphapos.js
@@ -1,8 +1,6 @@
 // Problem state; we will replace each letter with its position in the alphabet and return the output
+// Define parameters; We define a parameter to accept the input string, for any text that isn't a letter, we ignore it and dont return it
 
-
-
-// Define parameters
 //Give expected return if any
 // Give examples/tests
 // Give pseudo 
