f57819b582042d2b01f821702bf3d5c1fd76a8ee
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-17 10:52:30 +0300

    examples

--- alphapos.js
+++ alphapos.js
@@ -1,7 +1,8 @@
 // Problem state; we will replace each letter with its position in the alphabet and return the output
 // Define parameters; We define a parameter to accept the input string, for any text that isn't a letter, we ignore it and dont return it
 //Give expected return if any; we return the alphabet positions of the letters
-// Give examples/tests
+// Give examples/tests;  alphabetPosition('Abcde') --> '1 2 3 4 5'; alphabetPosition('fghij') --> '6 7 8 9 10'; alphabetPosition('klmno') --> '11 12 13 14 15'
+
 // Give pseudo 
 // Give solution code
 // Use test cases / log on console
