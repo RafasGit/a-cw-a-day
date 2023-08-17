755d2c16035997e4904dc9cbea1d88c3bc31e37c
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-17 10:46:40 +0300

    returns

--- alphapos.js
+++ alphapos.js
@@ -1,7 +1,6 @@
 // Problem state; we will replace each letter with its position in the alphabet and return the output
 // Define parameters; We define a parameter to accept the input string, for any text that isn't a letter, we ignore it and dont return it
-
-//Give expected return if any
+//Give expected return if any; we return the alphabet positions of the letters
 // Give examples/tests
 // Give pseudo 
 // Give solution code
