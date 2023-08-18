1248486a00ab8c240a5a6ce589bb40c499fae24e
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-18 09:14:35 +0300

    pseudo

--- strcml.js
+++ strcml.js
@@ -2,3 +2,4 @@
 // We are to declare a parameter that takes in a input string with each word separated by an underscore or dash.
 // We return the input string with the dash/underscores removed and the first characters of each word capitalized, if the first character is already capitalized it is returned as such
 // test cases; toCamelCase('my-name-is') // 'myNameIs' toCamelCase('We_Are_Alright') // 'WeAreAlright'  toCamelCase('We-made_it') // 'WeMadeIt'
+// We will split the input string into individual array elements, loop through the array and check to see if the element is a '-' or '_' using a condititonal, if so we capitalize the next element, we then join the returned elements
