79d407057f7616e5118d70c9d8e6a52b273fb5db
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-18 08:59:21 +0300

    return

--- strcml.js
+++ strcml.js
@@ -1,2 +1,3 @@
 // We are to convert dash or underscore delimited wordsinto camel casing.
 // We are to declare a parameter that takes in a input string with each word separated by an underscore or dash.
+// We return the input string with the dash/underscores removed and the first characters of each word capitalized, if the first character is already capitalized it is returned as such
