c04a3e330e6f127abb05e8b71f7ea2bc7fb0590d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-18 08:56:08 +0300

    parameters

--- strcml.js
+++ strcml.js
@@ -1,1 +1,2 @@
 // We are to convert dash or underscore delimited wordsinto camel casing.
+// We are to declare a parameter that takes in a input string with each word separated by an underscore or dash.
