8b6f63924177b158caa153a5ad4d212493d089d0
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-18 09:15:32 +0300

    solution

--- strcml.js
+++ strcml.js
@@ -3,3 +3,16 @@
 // We return the input string with the dash/underscores removed and the first characters of each word capitalized, if the first character is already capitalized it is returned as such
 // test cases; toCamelCase('my-name-is') // 'myNameIs' toCamelCase('We_Are_Alright') // 'WeAreAlright'  toCamelCase('We-made_it') // 'WeMadeIt'
 // We will split the input string into individual array elements, loop through the array and check to see if the element is a '-' or '_' using a condititonal, if so we capitalize the next element, we then join the returned elements
+
+function toCamelCase(str){
+    str = str.split('');
+    return str.map(function(el, i){
+      if(el == '-' || el == '_'){
+        el = str[i+1].toUpperCase();
+        str.splice(i+1, 1);
+      }
+     return el;
+      
+    }).join('');
+  }
+  
