2a0fb2833a198c36da27153b1ed7d978c941685d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-19 21:57:48 +0300

    draft

--- pangram.js
+++ pangram.js
@@ -1,5 +1,6 @@
 // We are to detect if a sentence is a pangram, it is a pangram if it contains every single letter in the alphabet
 // We are to define parameters
 //We give the return value
-// We give the examples or test cases
+// We give the examples or test cases eg1;
 // We will then give our pseudocode
+
