5c8e65fd0bcf24b762bba41b4c8c498a66da1724
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-20 17:37:02 +0300

    parameters

--- pangram.js
+++ pangram.js
@@ -1,6 +1,6 @@
 // We are to detect if a sentence is a pangram, it is a pangram if it contains every single letter in the alphabet
 // We are to define parameters; we define a parameter to accept a passed on string, numbers and punctuation are to be ignored
-//We give the return value
+//We give the return value; we will return True if sentence contains all letters of the alphabet and returns false if it doesnt contain all alphabets
 // We give the examples or test cases eg1; eg2; eg3;
 // We will then give our pseudocode
 // We then give our solution code
