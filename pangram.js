3f86684cefa40426c24aa5fb309fbe80fb20685e
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-20 17:56:06 +0300

    pseudo

--- pangram.js
+++ pangram.js
@@ -2,7 +2,8 @@
 // We are to define parameters; we define a parameter to accept a passed on string, numbers and punctuation are to be ignored
 //We give the return value; we will return True if sentence contains all letters of the alphabet and returns false if it doesnt contain all alphabets
 // We give the examples or test cases eg1; isPangram('my name is jeff') //false eg2; isPangram(the quick brown fox jumps over the lazy dog') //true eg3; isPangram('33;;22') // false
-// We will then give our pseudocode
+// We will then give our pseudocode; we declare an array of all the alphabet characters, we then compare our input string to check if the index of all characters referenced from our alphabet array are greater than zero
+//
 // We then give our solution code
 
 //We then define our test cases
