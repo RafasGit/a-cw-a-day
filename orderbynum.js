9f9910061c27fa9331c5c8e3fb259a086f3a7f72
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-21 22:44:00 +0300

    draft

--- orderbynum.js
+++ orderbynum.js
@@ -1,3 +1,4 @@
 // We are to sort a given string in ascending order with each number in each string the determining factor for sorting
 //We describe the parameters that we will define
 //We declare our expected return values
+// We also give our examples/ test cases
