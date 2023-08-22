7466b914b448e47e2c3a54a5e748b0f7a0d119cb
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-22 13:51:09 +0300

    parameters

--- orderbynum.js
+++ orderbynum.js
@@ -1,7 +1,7 @@
 // We are to sort a given string in ascending order with each number in each word the determining factor for sorting
 //We describe the parameters that we will define; we will define a parameter that will take in the input string
 
-//We declare our expected return values
+//We declare our expected return values; we are to return the input string having been sorted with the word with the smallest integer leading
 // We also give our examples/ test cases
 // We will then give our pseudocode to walkthrough our solution
 // We then give our solution code written in js
