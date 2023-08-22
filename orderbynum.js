9006cad995c96854fe856034ea7a56b192813b3d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-22 14:23:33 +0300

    solution

--- orderbynum.js
+++ orderbynum.js
@@ -6,6 +6,19 @@
 // We will then give our pseudocode to walkthrough our solution; 
     // We split the string into array elements, create a new array, use nested for loops to see if the element indexed has a number property, if so we push it into our new array
 // We then give our solution code written in js
+function order(words){
+    var array = words.split(' ');
+    var sortedArray = [];
+    for(i = 0; i <= array.length; i++) {
+      for(j = 0; j < array.length; j++) {
+        if(array[j].indexOf(i) >= 0) {
+          sortedArray.push(array[j]);
+        }
+      }
+    }
+    return sortedArray.join(' ');
+  }
+  
 // We then give our example test cases;
  
 // 1st test case;
