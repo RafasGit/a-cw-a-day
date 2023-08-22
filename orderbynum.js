89309cec6b0e30e57b821b3d4eae7e359ca6e88d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-22 13:55:04 +0300

    tests

--- orderbynum.js
+++ orderbynum.js
@@ -2,7 +2,7 @@
 //We describe the parameters that we will define; we will define a parameter that will take in the input string
 
 //We declare our expected return values; we are to return the input string having been sorted with the word with the smallest integer leading
-// We also give our examples/ test cases; order('th3e boy2 is6 goin1g t0o mak5e i8t'), order('w2e ha3ve to4'), order
+// We also give our examples/ test cases; order('th3e boy2 is6 goin1g t0o mak5e i8t'), order('w2e ha3ve to4'), order('kee4p push5ing br3o')
 // We will then give our pseudocode to walkthrough our solution
 // We then give our solution code written in js
 // We then give our example test cases;
