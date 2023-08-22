4252861fad8ab1189b0ab388a6a58302786a153c
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-22 14:22:29 +0300

    pseudo

--- orderbynum.js
+++ orderbynum.js
@@ -3,7 +3,8 @@
 
 //We declare our expected return values; we are to return the input string having been sorted with the word with the smallest integer leading
 // We also give our examples/ test cases; order('th3e boy2 is6 goin1g to0o mak5e i8t') // --> 'to0o goin1g boy2 th3e mak5e i6s i8t', order('w2e ha3ve to4') //--> 'w2e ha3ve to4', order('kee4p push5ing br3o') // --> 'kee4p push5ing br3o'
-// We will then give our pseudocode to walkthrough our solution
+// We will then give our pseudocode to walkthrough our solution; 
+    // We split the string into array elements, create a new array, use nested for loops to see if the element indexed has a number property, if so we push it into our new array
 // We then give our solution code written in js
 // We then give our example test cases;
  
