261547cfc4e3d7da513264ffb58ade908dfdbec3
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-23 08:36:28 +0300

    draft

--- stringsplit.js
+++ stringsplit.js
@@ -1,4 +1,4 @@
 // We will define the problem to be solved
 //We will then define our parameters
 // We then declare our return value
-// We then give our test cases; eg1; , eg2: ,
+// We then give our test cases; eg1; , eg2: ,eg3: .
