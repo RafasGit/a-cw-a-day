bae280403686da710f24a4100bacd50dcfcd049d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-24 21:27:33 +0300

    parameter

--- stringsplit.js
+++ stringsplit.js
@@ -1,7 +1,8 @@
 // We will define the problem to be solved; we are to accept a string and return it having been split into pairs of characters and for odd numbered characters, return ('_') with the final pair
 //We will then define our parameters; we will define a parameter that accepts our string to split 
   
-// We then declare our return value
+// We then declare our return value; we will return our input string having been split into pairs of characters
+ 
 // We then give our test cases; eg1; , eg2: ,eg3: .
 // Explain the solution in terms of pseudocode
 // Give actual solution code
