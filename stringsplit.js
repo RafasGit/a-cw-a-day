0942b6f0fedfddc827d79b27cf2587e8427d5c6e
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-24 21:20:00 +0300

    draft

--- stringsplit.js
+++ stringsplit.js
@@ -1,4 +1,4 @@
-// We will define the problem to be solved
+// We will define the problem to be solved; we are to accept a string and return it having been split into pairs of characters and for odd numbered characters, return ('_') with the final pair
 //We will then define our parameters
 // We then declare our return value
 // We then give our test cases; eg1; , eg2: ,eg3: .
