0e12ec5f11e91c2d18ae58cd171cf1a9c5a66224
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-27 16:29:52 +0300

    examples

--- breakcamel.js
+++ breakcamel.js
@@ -1,7 +1,7 @@
 // We are to come up with a solution that splits a string with camel casing using space between the words
 // We will define the parameters that will take in our input; our parameter accepts a string , empty string and with no integers accepted 
 // We will then explain our expected return output; we will return a string with the camel cased words spaced 
-// We give our examples to act us our test cases; eg solution('guickBrown') //--> guickBriwn;  solution('brownBoy')//--> brown Boy
+// We give our examples to act us our test cases; eg solution('guickBrown') //--> guickBriwn;  solution('brownBoy')//--> brown Boy; solution(richBoy) // --> rich Boy
 
 // We will then explain our solution without code; our pseudocode
 // We test out our edge cases
