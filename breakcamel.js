423f782137f2339ce7567ad69231d03b9848fb1a
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-27 16:53:44 +0300

    solution

--- breakcamel.js
+++ breakcamel.js
@@ -3,6 +3,20 @@
 // We will then explain our expected return output; we will return a string with the camel cased words spaced 
 // We give our examples to act us our test cases; eg solution('guickBrown') //--> guickBriwn;  solution('brownBoy')//--> brown Boy; solution(richBoy) // --> rich Boy
 
-// We will then explain our solution without code; our pseudocode; we split the string into array elements, loop through to check if any character is capitalzized (toUpperCase is either true or false); if true we add a space to a new array then add the character
+// We will then explain our solution without code; our pseudocode; we split the string into array elements, loop through to check if any character is capitalzized (toUpperCase is either true or false); if true we add a space to a new string then add the character
+// We give our solution 
+function solution(string) {
+    let cameld = '';
+  for(letter of string){
+    if(letter == letter.toUpperCase()){
+      cameld +=' '
+      cameld += letter
+    }
+    else
+      cameld += letter;
+  }
+  return cameld;
+}
+
 
 // We test out our edge cases
