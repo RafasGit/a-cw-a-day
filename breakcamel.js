7668dcc679c103dff0dfcde59080c9433a466f73
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-27 16:13:03 +0300

    parameter

--- breakcamel.js
+++ breakcamel.js
@@ -1,6 +1,6 @@
 // We are to come up with a solution that splits a string with camel casing using space between the words
 // We will define the parameters that will take in our input; our parameter accepts a string , empty string and with no integers accepted 
-// We will then explain our expected return output
+// We will then explain our expected return output; we will return a string with the camel cased words spaced 
 // We give our examples to act us our test cases
 // We will then explain our solution without code; our pseudocode
 // We test out our edge cases
