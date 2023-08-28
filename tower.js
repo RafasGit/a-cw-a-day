a2ae6730f99b77605bb53f03dbad6ed4e5adba51
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-28 23:37:16 +0300

    draft

--- tower.js
+++ tower.js
@@ -1,4 +1,4 @@
 // We are going to define our problem to solve
 // We will then define the parameters we are to declare
 // We then give the return value that we are to expect
-// We will then give our examples to be used as test cases;
+// We will then give our examples to be used as test cases; eg1;
