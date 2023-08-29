311a0f926bbce81acb272e26716d1e9c85e9e798
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-29 23:12:11 +0300

    new tower

--- tower.js
+++ tower.js
@@ -1,4 +1,4 @@
-// We are going to define our problem to solve
+// We are going to define our problem to solve; we are to create a tower out of '*' strings with the tower increasing in size every column 
 // We will then define the parameters we are to declare
 // We then give the return value that we are to expect
 // We will then give our examples to be used as test cases; eg1; eg2; eg3;
