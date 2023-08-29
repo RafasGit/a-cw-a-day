9d15bc09fd3f894c69d2b68d902202343cbcd568
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-29 23:23:26 +0300

    return

--- tower.js
+++ tower.js
@@ -1,6 +1,6 @@
 // We are going to define our problem to solve; we are to create a tower out of '*' strings with the tower increasing in size every row for the number of rows dictated by input integer
 // We will then define the parameters we are to declare; we are to define a parameter that will accept the input integer for required number of tower rows
-// We then give the return value that we are to expect
+// We then give the return value that we are to expect; we are to return the tower of asterisks in string format
 // We will then give our examples to be used as test cases; eg1; eg2; eg3;
 // We then give our pseudocode explaining how we will solve the issue using non-technical terms
 // We give our solution code
