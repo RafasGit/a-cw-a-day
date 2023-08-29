cf14f09cc18613fef7d6d319d6e323f38b3c8fb0
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-29 23:27:43 +0300

    examples

--- tower.js
+++ tower.js
@@ -1,7 +1,7 @@
 // We are going to define our problem to solve; we are to create a tower out of '*' strings with the tower increasing in size every row for the number of rows dictated by input integer
 // We will then define the parameters we are to declare; we are to define a parameter that will accept the input integer for required number of tower rows
 // We then give the return value that we are to expect; we are to return the tower of asterisks in string format
-// We will then give our examples to be used as test cases; eg1; eg2; eg3;
+// We will then give our examples to be used as test cases; eg1; towerBuilder(1) // ["*"] eg2; towerBuilder(2) // ["*","***"] eg3; towerBuilder(4) //["*","***","*****","*******"] 
 // We then give our pseudocode explaining how we will solve the issue using non-technical terms
 // We give our solution code
 // We then test our examples
