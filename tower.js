7b89bc360a7db7e470068add3e4aeafc16473378
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-29 23:22:21 +0300

    new tower

--- tower.js
+++ tower.js
@@ -1,4 +1,4 @@
-// We are going to define our problem to solve; we are to create a tower out of '*' strings with the tower increasing in size every row
+// We are going to define our problem to solve; we are to create a tower out of '*' strings with the tower increasing in size every row for the number of rows dictated by input integer
 // We will then define the parameters we are to declare; we are to define a parameter that will accept the input integer for required number of tower rows
 // We then give the return value that we are to expect
 // We will then give our examples to be used as test cases; eg1; eg2; eg3;
