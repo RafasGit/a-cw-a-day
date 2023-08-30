3358563a38faaf918c350083b1691ca2352ef01a
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-30 14:36:00 +0300

    oddint.js

--- oddint.js
+++ oddint.js
@@ -2,3 +2,4 @@
 // We define the parameters we are to create to accept inputs
 // We also state what our function will return
 // We then give example scenarios of using the solution; eg1(), eg2(), eg3()
+// We then define solution in non-technical terms in pseudocode format
