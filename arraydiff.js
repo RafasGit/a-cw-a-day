883594f00e26e1d851b6f4a3c734ebd0871033a3
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-08-31 23:59:57 +0300

    arraydiff.js

--- arraydiff.js
+++ arraydiff.js
@@ -3,4 +3,5 @@
 // We then give our expected return value
 // We then define our examples to be tested; eg1(), eg2(), eg3()
 // We will define our problem solution using our pseudocode
+// We give our solution code 
 // We then test our examples
