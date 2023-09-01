f273baa33ac57e4a92e37354f3752bbb352727b0
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-01 18:15:44 +0300

    problem state

--- arraydiff.js
+++ arraydiff.js
@@ -1,7 +1,13 @@
-// We define our problem to solve
+// We define our problem to solve; we are given two arrays and we are two return the values not repeated in both arrays
 // We define our parameters to accept inputs
 // We then give our expected return value
 // We then define our examples to be tested; eg1(), eg2(), eg3()
 // We will define our problem solution using our pseudocode
 // We give our solution code 
 // We then test our examples
+
+
+    // function arrayDiff(a, b) {
+    //     // Filter entire a array, only keep the value if b does not contain that value
+    //     return a.filter(val => !b.includes(val));
+    // }
