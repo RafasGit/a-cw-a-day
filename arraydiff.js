5a3d06c49c4854a4d582cafff7399258943d5afb
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-01 18:23:03 +0300

    examples

--- arraydiff.js
+++ arraydiff.js
@@ -3,7 +3,7 @@
 
 // We then give our expected return value ; we will return the array elements unique to each array
 
-// We then define our examples to be tested; eg1(), eg2(), eg3()
+// We then define our examples to be tested; eg1; arrayDiff([1,2], [1]) // [2], eg2; arrayDiff([3,4], [3,4]) // [], eg3; arrayDiff([1,2], [3,4])// [1,2] [3,4]
 // We will define our problem solution using our pseudocode
 // We give our solution code 
 // We then test our examples
