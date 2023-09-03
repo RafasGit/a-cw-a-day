151e514bfdb0afec6f5bc700b216fb83b03445bc
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-03 23:37:00 +0300

    draft

--- digitalroot.js
+++ digitalroot.js
@@ -2,3 +2,4 @@
 // We provide our parameters to accept inputs
 // We then define our return statement
 // We provide our examples to be used as test cases
+// We give our solution in form of non-technical jargon
