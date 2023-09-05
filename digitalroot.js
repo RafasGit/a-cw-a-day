67dcbad73e3e6f41a74dd51edb95bab617e5e139
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-05 23:47:14 +0300

    egs

--- digitalroot.js
+++ digitalroot.js
@@ -2,6 +2,6 @@
 // We provide our parameters to accept inputs; we will declare a parameter that will accept our integer to be summed
 // We then define our return statement we will return a single digit that is the end point of our recursive summation 
 // We provide our examples to be used as test cases; digitalRoot(256)  // --> 4 digitalRoot(304) // --> 7 digitalRoot(40) // --> 
-// We give our solution in form of non-technical jargon
+// We give our solution in form of non-technical jargon; function checks to see if input number is lesser than 10, if lesser we return the input element else we split the number into individual array elements; then reduce the array elements
 // We then give our solution code
 // We test out our example test cases
