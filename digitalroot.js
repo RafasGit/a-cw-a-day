13ef8276d457acfbd4b733f921864a79164e9ea8
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-05 22:38:36 +0300

    egs

--- digitalroot.js
+++ digitalroot.js
@@ -1,7 +1,7 @@
 // We are to provide our problem to solve; we are to find the recursive sum of all digits in a number
 // We provide our parameters to accept inputs; we will declare a parameter that will accept our integer to be summed
 // We then define our return statement we will return a single digit that is the end point of our recursive summation 
-// We provide our examples to be used as test cases
+// We provide our examples to be used as test cases; digitalRoot(256)  // --> 4 digitalRoot(304) // --> 7 digitalRoot(40) // --> 
 // We give our solution in form of non-technical jargon
 // We then give our solution code
 // We test out our example test cases
