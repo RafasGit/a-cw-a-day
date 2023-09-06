bebadac436abc2d6425ca187c0ba5f582bd95f81
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-06 19:44:08 +0300

    draft

--- bitcount.js
+++ bitcount.js
@@ -1,3 +1,4 @@
 // We describe the problem our solution is to solve
 // we explain our parameters and the inputs they will take in
 // We then give what we expect to be our return value.
+// We then give our examples to be used as test cases
