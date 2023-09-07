4c06c1dad770d93b812ce48d58991286f897a730
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-07 15:15:46 +0300

    example

--- bitcount.js
+++ bitcount.js
@@ -2,6 +2,6 @@
 // we explain our parameters and the inputs they will take in; we define a parameter that takes in a number 
 // We then give what we expect to be our return value; we return our count of binary representations that equals one 
 // We then give our examples to be used as test cases; countBits(0) // --> 0; countBits(4) // --> 1; countBits(10) // --> 2;
-// We give our pseudocode explaining non-technically our solution
+// We give our pseudocode explaining non-technically our solution; we convert each number input into string to get its binary representation, loop through and if its binary representation is equal to one, we add to our variable count. 
 // We give our solution code
 // We then test out out examples
