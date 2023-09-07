e002916c771fb83f7ac5012a5b15f6603dc70a00
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-07 15:01:16 +0300

    prob state

--- bitcount.js
+++ bitcount.js
@@ -1,4 +1,4 @@
-// We describe the problem our solution is to solve
+// We describe the problem our solution is to solve; we are to create a solution that takes in an integer input and returns number of bits equal to one in binary representation of the number
 // we explain our parameters and the inputs they will take in
 // We then give what we expect to be our return value.
 // We then give our examples to be used as test cases
