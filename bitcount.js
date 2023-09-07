369d2cbb808f6d7606a425a4c32a32d5673499e3
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-07 15:04:15 +0300

    parameters

--- bitcount.js
+++ bitcount.js
@@ -1,5 +1,5 @@
 // We describe the problem our solution is to solve; we are to create a solution that takes in an integer input and returns number of bits equal to one in binary representation of the number
-// we explain our parameters and the inputs they will take in
+// we explain our parameters and the inputs they will take in; we define a parameter that takes in a number 
 // We then give what we expect to be our return value.
 // We then give our examples to be used as test cases
 // We give our pseudocode explaining non-technically our solution
