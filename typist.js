2d6e8fa1b160131244463e411c9f691ec0e0a301
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-08 20:42:12 +0300

    draft

--- typist.js
+++ typist.js
@@ -1,3 +1,4 @@
 // We describe the problem our solution is to solve
 // We then describe the parameters we will define and the input it takes in
 // We describe our expected return parameter
+// We then give our examples
