e477dc64b38472531c933e513c1db9689563821f
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-10 01:49:06 +0300

    pseudo

--- typist.js
+++ typist.js
@@ -3,8 +3,22 @@
 // We describe our expected return value; we return an integer which is the number of times the keyboard is touched
 // We then give our examples typist('fdg') // --> 3; typist('Ghe') // --> 5; typist('G') // --> 3
 // We descirbe our code solution using non-technical terms; we accept the string input, loop through the string characters, check if the character is upper or lower case, the count is updated by 2 else count is updated by one
-
 // We give the actual code solution
+function typist(s){
+    let res = s.length
+    let caps = false
+    for (let i = 0; i < s.length; i++) {
+      if (s.charCodeAt(i) < 97 && caps === false) {
+        res++
+        caps = true
+      }
+      else if (s.charCodeAt(i) > 96 && caps === true) {
+      res++
+      caps = false
+      }
+    }
+    return res
+  }
 // We test out our examples using test cases
 
 console.log()
