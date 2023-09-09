83fbcff4efdf0caafd79023e24c8f62da160d4f5
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-09 23:10:42 +0300

    return

--- typist.js
+++ typist.js
@@ -1,6 +1,6 @@
 // We describe the problem our solution is to solve; we are to count the number of times an individual clicks on his keyboard
 // We then describe the parameters we will define and the input it takes in; we define a parameter to take in the input string
-// We describe our expected return parameter
+// We describe our expected return value; we return an integer which is the number of times the keyboard is touched
 // We then give our examples
 // We descirbe our code solution using non-technical terms
 // We give the actual code solution
