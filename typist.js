8b73264fe0ccd2f4d4a98f49867650a6d6f846b9
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-09 23:59:50 +0300

    exmples

--- typist.js
+++ typist.js
@@ -2,7 +2,7 @@
 // We then describe the parameters we will define and the input it takes in; we define a parameter to take in the input string
 // We describe our expected return value; we return an integer which is the number of times the keyboard is touched
 // We then give our examples typist('fdg') // --> 3; typist('Ghe') // --> 5; typist('G') // --> 3
-// We descirbe our code solution using non-technical terms; we accept the string input, loop through the string characters
+// We descirbe our code solution using non-technical terms; we accept the string input, loop through the string characters, check if the character is upper or lower case
 
 // We give the actual code solution
 // We test out our examples using test cases
