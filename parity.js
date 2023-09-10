ed26353380fa4484ae84c973da6e18eb623dc787
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-10 23:44:54 +0300

    draft

--- parity.js
+++ parity.js
@@ -1,3 +1,4 @@
 // We describe the problem we are to solve 
 // We then define the parameters we would like to assign our variable input 
 // We give our expected return value
+// We give our examples to be used as test cases
