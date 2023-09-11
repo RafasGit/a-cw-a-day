a423c3dd7d55e974993ef59b3ffd9ee7594120d0
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-11 23:21:05 +0300

    tests

--- parity.js
+++ parity.js
@@ -1,7 +1,7 @@
 // We describe the problem we are to solve; we are to find the parity outlier that is either odd in an array of even numbers or even amongst an array of odd numbers
 // We then define the parameters we would like to assign our variable input; we define a parameter that takes in an input of an array of numbers
 // We give our expected return value; we return the integer that is even amongst odd numbers or the odd number amongst even integers
-// We give our examples to be used as test cases eg1; eg2; eg3;
+// We give our examples to be used as test cases eg1; eg2; eg3; findOutlier(2, 4, 5, 8, 12) // --> 5
 // We describe our solution in pseudocode; giving non technical procedure of solving
 // We give our actual solution code
 // We give our test cases 
