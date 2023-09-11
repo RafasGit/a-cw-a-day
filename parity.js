117038126e5bef3616b544bfe304831783b676cf
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-11 23:16:32 +0300

    returns

--- parity.js
+++ parity.js
@@ -1,5 +1,5 @@
 // We describe the problem we are to solve; we are to find the parity outlier that is either odd in an array of even numbers or even amongst an array of odd numbers
-// We then define the parameters we would like to assign our variable input 
+// We then define the parameters we would like to assign our variable input; we define a parameter that takes in an input of an array of numbers
 // We give our expected return value
 // We give our examples to be used as test cases eg1; eg2; eg3;
 // We describe our solution in pseudocode; giving non technical procedure of solving
