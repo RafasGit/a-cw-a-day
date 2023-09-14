2606800a5856601229393240719c226cc490c472
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-14 22:20:05 +0300

    egs

--- walkten.js
+++ walkten.js
@@ -1,7 +1,7 @@
 // We define the problem statement; we accept an aray of keyed in compass directions and confirm if the directions return us to our starting pointand after 10 minutes
 // We define our parameters; we define parameters that will take in the input directions given by the walking app; we will not receive an empty input at any point
 // We define our return value; we will return true if directions given return walker to starting point and after 10 minutes else we return false
-// We give our example statements eg1; isValidWalk(['n'.'n'.'s'.'e']) // --> false eg2; isValidWalk(['n', 's', 'w', 'e']) // --> true eg3... 
+// We give our example statements eg1; isValidWalk(['n'.'n'.'s'.'e']) // --> false eg2; isValidWalk(['n', 's', 'w', 'e']) // --> true eg3... isValidWalk(['e']) //--> false
 // We give our pseudocode
 // We give our solution code
 // We test out our examples
