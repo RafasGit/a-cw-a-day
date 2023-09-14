55c70a3688321d5006d486d7bd06e7544bea017b
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-14 21:53:03 +0300

    Problem statement

---
+++ walkten.js
@@ -0,0 +1,11 @@
+// We define the problem statement; we accept an input of keyed in compass directions and confirm if the directions return us to our starting pointand after 10 minutes
+// We define our parameters
+// We define our return value
+// We give our example statements eg1; eg2; eg3...
+// We give our pseudocode
+// We give our solution code
+// We test out our examples
+
+//eg1 console.log()
+//eg2 console.log()
+//eg3 console.log()
