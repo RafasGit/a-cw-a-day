fcf12b617835585052cc46a27329bdc48bdf0342
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-14 22:03:25 +0300

    returns

--- walkten.js
+++ walkten.js
@@ -1,6 +1,6 @@
 // We define the problem statement; we accept an input of keyed in compass directions and confirm if the directions return us to our starting pointand after 10 minutes
 // We define our parameters; we define parameters that will take in the input directions given by the walking app
-// We define our return value
+// We define our return value; we will return true if directions given return walker to starting point and after 10 minutes else we return false
 // We give our example statements eg1; eg2; eg3...
 // We give our pseudocode
 // We give our solution code
