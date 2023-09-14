86b3385a32f16523b1e0b66c4b5a00ef1874ab06
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-14 22:24:50 +0300

    pseudo

--- walkten.js
+++ walkten.js
@@ -2,7 +2,7 @@
 // We define our parameters; we define parameters that will take in the input directions given by the walking app; we will not receive an empty input at any point
 // We define our return value; we will return true if directions given return walker to starting point and after 10 minutes else we return false
 // We give our example statements eg1; isValidWalk(['n'.'n'.'s'.'e']) // --> false eg2; isValidWalk(['n', 's', 'w', 'e']) // --> true eg3... isValidWalk(['e']) //--> false
-// We give our pseudocode; we check if distance covered forward(North) is cancelled out by distance going backwards(south) and if distance going left(west) is canceled out by distance going right(east)
+// We give our pseudocode; we check if distance covered forward(North) is cancelled out by distance going backwards(south) and if distance going left(west) is canceled out by distance going right(east), steps taken should amount to ten
 // We give our solution code
 // We test out our examples
 
