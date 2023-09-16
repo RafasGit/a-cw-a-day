d1142368292b038f356dd714fdeecf907f92272d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-16 23:55:17 +0300

    draft

--- morsedecode.js
+++ morsedecode.js
@@ -1,3 +1,4 @@
 // We define the problem statement's requirement to be solved
 // We discuss our parameters to be defined and the inputs to be accepted
 // We then define  our return values
+// We give our examples to be used 
