864ecc2311954b49f8f6050f80350b77452e2917
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-17 23:19:34 +0300

    test

--- morsedecode.js
+++ morsedecode.js
@@ -3,5 +3,16 @@
 // We then define our return values; we will return the deciphered morse code in string format
 // We give our examples to be used; eg1, decodeMorse(' .... . -.-- ') //--> 'HEY' eg2, decodeMorse('.... . -.--   .--- ..- -.. .'), 'HEY JUDE' eg3
 // We then give our pseudocode to be used as a non-technical breakdown of our solution; We call our morsecode function in which we will loop through each code to decipher each letter. We then define a function to split the input into words and pass the words into our third function, which split the words into individual letters then loop through individual letters into the morsecode function
-// We then give our solution code
+// We then give our solution code; 
+
+decodeMorse = function(morseCode){
+    function decodeMorseLetter(letter) {
+      return MORSE_CODE[letter];
+    }
+    function decodeMorseWord(word) {
+      return word.split(' ').map(decodeMorseLetter).join('');
+    }
+    return morseCode.trim().split('   ').map(decodeMorseWord).join(' ');
+  }
 // We give our test case examples
+ console.log(decodeMorse(' .... . -.-- ')) // --> 'HEY'
