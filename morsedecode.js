50eaa7d1ee3b85077f22b7c16939661803eeaf6e
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-17 23:00:58 +0300

    egs

--- morsedecode.js
+++ morsedecode.js
@@ -1,7 +1,7 @@
 // We define the problem statement's requirement to be solved; we take in morse code and return the deciphered string 
 // We discuss our parameters to be defined and the inputs to be accepted; we accept a parameter to accept the morsecode input. the input is not to be an empty string and will include unnecesary spacing between the morse symbols.  
 // We then define our return values; we will return the deciphered morse code in string format
-// We give our examples to be used; eg1, eg2, eg3
+// We give our examples to be used; eg1, decodeMorse(' .... . -.-- ') //--> 'HEY' eg2, eg3
 // We then give our pseudocode to be used as a non-technical breakdown of our solution
 // We then give our solution code
 // We give our test case examples
