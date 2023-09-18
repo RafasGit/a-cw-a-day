a6c67b6bde1360963fe68916b39adf856e50f24a
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-18 15:51:16 +0300

    draft

--- kebabize.js
+++ kebabize.js
@@ -1,7 +1,7 @@
 // We describe our problem statement to be solved
 // We then describe our parameter to accept inputs
 // We describe our return value that is to be expected
-// We give our examples to test the solution
+// We give our examples to test the solution eg1, eg2, eg3
 // We then give our pseudocode to breakdown our solution in simple terms
 // We give our solution code
 // We test out our test cases
