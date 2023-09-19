72c9f55638ec8ded9b03e31eb6895aadd1c80fd0
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-19 23:12:50 +0300

    prob statement

--- kebabize.js
+++ kebabize.js
@@ -1,4 +1,4 @@
-// We describe our problem statement to be solved
+// We describe our problem statement to be solved; we are to create a code that takes in camel case text and returns it in kebab case
 // We then describe our parameter to accept inputs
 // We describe our return value that is to be expected
 // We give our examples to test the solution eg1, eg2, eg3
