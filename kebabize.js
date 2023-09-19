f254246420a0435686e5df9abd37854c202201da
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-19 23:20:53 +0300

    egs

--- kebabize.js
+++ kebabize.js
@@ -1,7 +1,7 @@
 // We describe our problem statement to be solved; we are to create a code that takes in camel case text and returns it in kebab case
 // We then describe our parameter to accept inputs; we initialize a parameter to accept a string in camel case
 // We describe our return value that is to be expected; we return the string in kebab format
-// We give our examples to test the solution eg1, eg2, eg3
+// We give our examples to test the solution eg1, kebabize('NGOMBE') // --> 'n-g-o-m-b-e' eg2, eg3
 // We then give our pseudocode to breakdown our solution in simple terms
 // We give our solution code
 // We test out our test cases
