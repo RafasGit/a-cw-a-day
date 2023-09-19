3c926ff0825e42631cb92d6297fec19e20f1c97b
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-19 23:59:23 +0300

    egs

--- kebabize.js
+++ kebabize.js
@@ -2,6 +2,6 @@
 // We then describe our parameter to accept inputs; we initialize a parameter to accept a string in camel case
 // We describe our return value that is to be expected; we return the string in kebab format
 // We give our examples to test the solution eg1, kebabize('NGOMBE') // --> 'n-g-o-m-b-e' eg2, kebabize('makeMoney') // --> 'make-money' eg3 kebabize('doTheThing') // --> 'do-the-thing'
-// We then give our pseudocode to breakdown our solution in simple terms
+// We then give our pseudocode to breakdown our solution in simple terms; we split the string into individual elements, we then filter to find the characters that are not equal to uppercase and join the text, 
 // We give our solution code
 // We test out our test cases
