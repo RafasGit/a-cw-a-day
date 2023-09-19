b5a6d005eb8e679dce7f994a6fbeb9a197236af9
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-19 23:14:07 +0300

    parameter

--- kebabize.js
+++ kebabize.js
@@ -1,5 +1,5 @@
 // We describe our problem statement to be solved; we are to create a code that takes in camel case text and returns it in kebab case
-// We then describe our parameter to accept inputs
+// We then describe our parameter to accept inputs; we initialize a parameter to accept a string in camel case
 // We describe our return value that is to be expected
 // We give our examples to test the solution eg1, eg2, eg3
 // We then give our pseudocode to breakdown our solution in simple terms
