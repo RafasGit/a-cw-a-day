fd1fa89852aa98b84d3f85f457ac2575ac695767
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-21 18:51:04 +0300

    return

--- persistent.js
+++ persistent.js
@@ -1,6 +1,6 @@
 // We define the statement that requires a solution; we take a number input and return a single digit only after having multiplied the positive parameter 
 // We then define the parameters we are to define; we are to define a parameter that takes in the input integer
-// We then give our return value
+// We then give our return value; we return an integer indicating the number of times it has taken to get a single digit
 // We then give our example statements
 // We give our pseudocode explanation
 // We give our examples to be used as test cases
