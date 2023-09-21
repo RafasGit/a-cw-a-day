fec9a8c0e59abcd4431dfaa10a7d7771953cc158
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-21 18:49:54 +0300

    parameter

--- persistent.js
+++ persistent.js
@@ -1,5 +1,5 @@
 // We define the statement that requires a solution; we take a number input and return a single digit only after having multiplied the positive parameter 
-// We then define the parameters we are to define
+// We then define the parameters we are to define; we are to define a parameter that takes in the input integer
 // We then give our return value
 // We then give our example statements
 // We give our pseudocode explanation
