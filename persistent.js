46a0daf1196083b0b4db6b91ddd4c71979503f6c
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-21 23:16:23 +0300

    egs

--- persistent.js
+++ persistent.js
@@ -2,5 +2,5 @@
 // We then define the parameters we are to define; we are to define a parameter that takes in the input integer
 // We then give our return value; we return an integer indicating the number of times it has taken to get a single digit
 // We then give our example statements; persistence(42) // --> 1 persistence(38) // --> 2 persistence(4) // --> 2
-// We give our pseudocode explanation; we convert to string, we then split into two elements
+// We give our pseudocode explanation; we convert to string, we then split into two elements, multiply and if greater than 9, we multiply the two numbers once more until single digit is the result of a product
 // We give our examples to be used as test cases
