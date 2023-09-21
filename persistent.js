8755dbf5be0398a7a09de63ef5bed06e10f8c40b
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-21 18:49:08 +0300

    prob statement

--- persistent.js
+++ persistent.js
@@ -1,4 +1,4 @@
-// We define the statement that requires a solution 
+// We define the statement that requires a solution; we take a number input and return a single digit only after having multiplied the positive parameter 
 // We then define the parameters we are to define
 // We then give our return value
 // We then give our example statements
