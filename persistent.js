293d7b391ff7d95ea241674d8d7db4d7118bc5ff
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-23 20:28:10 +0300

    solution

--- persistent.js
+++ persistent.js
@@ -2,5 +2,13 @@
 // We then define the parameters we are to define; we are to define a parameter that takes in the input integer
 // We then give our return value; we return an integer indicating the number of times it has taken to get a single digit
 // We then give our example statements; persistence(42) // --> 1 persistence(38) // --> 2 persistence(4) // --> 2
-// We give our pseudocode explanation; we convert to string, we then split into two elements, multiply and if greater than 9, we multiply the two numbers once more until single digit is the result of a product
+// We give our pseudocode explanation; we convert to string, we then split into two elements, multiply and if greater than 9, we multiply the two numbers once more until single digit is the result of a product; try different functions for splitting and reducing
 // We give our examples to be used as test cases
+
+  function persistence(num) {
+    for (var i = 0; num > 9; i++) {
+      num = num.toString().split('').reduce((t, c) => c * t);
+    }
+    return i;
+ } 
+  
