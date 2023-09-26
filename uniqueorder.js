52c100ac932e9c8700d97a1b9be2c0ca71c36948
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-26 22:59:59 +0300

    prob state

--- uniqueorder.js
+++ uniqueorder.js
@@ -1,7 +1,22 @@
-// We define the problem statement to be solved
+// We define the problem statement to be solved; we are to create a function that removes immediate duplicates from a string or series of integers and returns a non duplicate version
 // We then define the parameters 
 // We then indicate our return value
 // We then give examples to be used as test cases
-// We then give our non-technical explanation as pseudocode
+// We then give our non-technical explanation as pseudocode; last element = to value being compared
 // We then give our solution code
 // We test out our examples
+var uniqueInOrder = function (iterable) {
+    var arr = [];
+    
+    for(var i = 0; i < iterable.length;i++ ){
+    //check if there is a repeating letter
+    
+    if(iterable[i] !== iterable[i+1]){
+     //push the remaining value to arr 
+    arr.push(iterable[i]);
+    
+             }
+    
+         }
+    return arr;
+    }
