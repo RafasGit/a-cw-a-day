12104a1dad26a94eabce4ca9b9cb58dfb2f6bf5b
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-26 23:08:02 +0300

    returns

--- uniqueorder.js
+++ uniqueorder.js
@@ -1,6 +1,6 @@
 // We define the problem statement to be solved; we are to create a function that removes immediate duplicates from a string or series of integers and returns a non duplicate version
 // We then define the parameters; we will defne a parameter that takes in our string or integer input  
-// We then indicate our return value
+// We then indicate our return value; we return a string or integers without any immediate repeat characters
 // We then give examples to be used as test cases
 // We then give our non-technical explanation as pseudocode; last element = to value being compared
 // We then give our solution code
