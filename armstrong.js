c67a239c5ebdac265e53006ff02bbc4c84c8216b
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-28 22:36:47 +0300

    returns

--- armstrong.js
+++ armstrong.js
@@ -1,6 +1,6 @@
 // We will define the problem we are to solve; we are to intake an integer and determine if  it is narcissistic, i.e; the sum of its own digits each raised to the power of the number of digits is equal to the number itself
 // We will then declare our parameters; we will initialize a parameter that takes in the input number; no string or empty value is to be expected
-// We then declare our return statement
+// We then declare our return statement; we will return true for narcissisitic numbers and false for non armstrong numbers
 // We give our example cases; eg1; eg2; eg3;
 // We then give our non-technical explanation to the solution 
 // we give our code solution 
