53799136191ee62c3f7f29fc8715b25af57c0162
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-28 22:49:29 +0300

    pseudo

--- armstrong.js
+++ armstrong.js
@@ -2,6 +2,6 @@
 // We will then declare our parameters; we will initialize a parameter that takes in the input number; no string or empty value is to be expected
 // We then declare our return statement; we will return true for narcissisitic numbers and false for non armstrong numbers
 // We give our example cases; eg1; narcissisistic(10) // --> false ; narcissistic(153) // --> true ; narcissistic(89) // --> false
-// We then give our non-technical explanation to the solution 
+// We then give our non-technical explanation to the solution; we will split the integer into array elements; raise each element to the power of the length of the array and comfirm if sum of the resulting values is equal to input value
 // we give our code solution 
 // We test out our examples
