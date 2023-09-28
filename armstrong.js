34012e8747f51fe9dd0515776615862ea003dbb4
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-28 22:40:55 +0300

    egs

--- armstrong.js
+++ armstrong.js
@@ -1,7 +1,7 @@
 // We will define the problem we are to solve; we are to intake an integer and determine if  it is narcissistic, i.e; the sum of its own digits each raised to the power of the number of digits is equal to the number itself
 // We will then declare our parameters; we will initialize a parameter that takes in the input number; no string or empty value is to be expected
 // We then declare our return statement; we will return true for narcissisitic numbers and false for non armstrong numbers
-// We give our example cases; eg1; narcissisistic(10) // --> false ; narcissistic(153) eg2; eg3;
+// We give our example cases; eg1; narcissisistic(10) // --> false ; narcissistic(153) // --> true eg2; eg3;
 // We then give our non-technical explanation to the solution 
 // we give our code solution 
 // We test out our examples
