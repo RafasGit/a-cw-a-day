5059214ac2b5c75ed62247d4c400023e97109a73
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-09-28 22:35:39 +0300

    prob state

--- armstrong.js
+++ armstrong.js
@@ -1,5 +1,5 @@
 // We will define the problem we are to solve; we are to intake an integer and determine if  it is narcissistic, i.e; the sum of its own digits each raised to the power of the number of digits is equal to the number itself
-// We will then declare our parameters
+// We will then declare our parameters; we will initialize a parameter that takes in the input number; no string or empty value is to be expected
 // We then declare our return statement
 // We give our example cases; eg1; eg2; eg3;
 // We then give our non-technical explanation to the solution 
