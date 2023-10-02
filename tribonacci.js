32bb3f9b41feea554a2ab4b1bbbce6504bf1038a
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-03 00:19:59 +0300

    draft

--- tribonacci.js
+++ tribonacci.js
@@ -1,3 +1,4 @@
 // We will describe the problem we are to solve
 // We then describe the parameters we will define
 // We then give our return statement 
+// We give our examples to be used as tests
