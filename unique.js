fb58529af587a0cbda3ad86022d9e7b16e76d95d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-03 22:05:18 +0300

    prep

--- unique.js
+++ unique.js
@@ -1,4 +1,5 @@
 // We describe the problem our solution tackles
 // We describe the parameters we will declare
 // We then declare our expected return value
-// We then give our examoles
+// We then give our examples
+// We then give our pseudocode
