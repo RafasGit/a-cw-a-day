aea798ed67379919bec14f52335c0f538392c458
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-05 00:13:12 +0300

    parameter

--- unique.js
+++ unique.js
@@ -1,5 +1,5 @@
 // We describe the problem our solution tackles; we return the number in an array of numbers that is unique
-// We describe the parameters we will declare
+// We describe the parameters we will declare; we declare a parameter that will accept an array input of integers with no strings or empty inputs
 // We then declare our expected return value
 // We then give our examples
 // We then give our pseudocode
