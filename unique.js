fb78b9d0aacc66cd579ce666df55cabd832d506c
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-05 00:16:34 +0300

    return

--- unique.js
+++ unique.js
@@ -1,7 +1,7 @@
 // We describe the problem our solution tackles; we return the number in an array of numbers that is unique
 // We describe the parameters we will declare; we declare a parameter that will accept an array input of integers with no strings or empty inputs
 // We then declare our expected return value; we return the unique number in the input array
-// We then give our examples
+// We then give our examples; findUniq([1, 2, 2, 4, 4]) // --> 1, findUniq([3, 3, 5, 6, 6]) // --> 5, findUniq([7, 7, 9, 10, 10]) // --> 9
 // We then give our pseudocode
 // We give our solution code
 
