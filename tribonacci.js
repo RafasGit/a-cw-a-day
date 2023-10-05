50260b83215f873db9df19d7537e57153af31522
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-05 23:59:59 +0300

    pseudo

--- tribonacci.js
+++ tribonacci.js
@@ -2,6 +2,6 @@
 // We then describe the parameters we will define
 // We then give our return statement 
 // We give our examples to be used as tests
-// We give our pseudocode solution; we first take in the array of values and the required length of the triboonacci sequence, we then check if the required sequence is 0, in which case we return 0 or if required sequence is 1, we return 1, we then loop through checking if
+// We give our pseudocode solution; we first take in the array of values and the required length of the triboonacci sequence, we then check if the required sequence is 0, in which case we return 0 or if required sequence is 1, we return 1, we then loop through checking if the value
 // We give our solution code 
 // We test out our examples
