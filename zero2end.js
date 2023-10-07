9430290885933ce310d449ae542fa46fd8e6bd5b
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-07 22:41:41 +0300

    prep

--- zero2end.js
+++ zero2end.js
@@ -1,7 +1,7 @@
 // We evaluate the problem we are to solve
 // We define the parameter we will initialize
 // We then describe the expected return values
-// We create examples to use as tests; eg1, 
+// We create examples to use as tests; eg1, eg2
 // We then describe the code in non-technical terms using pseudo
 // We then give our solution code
 // We then test our examples
