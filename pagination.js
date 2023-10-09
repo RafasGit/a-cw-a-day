2e7a4e2260e551e52a386277110aecefe85a25c2
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-09 23:59:15 +0300

    draft

--- pagination.js
+++ pagination.js
@@ -1,4 +1,4 @@
-// We discuss the problem we are to solve; we
+// We discuss the problem we are to solve; we ar
 // We define the parameters to accept inputs
 // We describe the return value expected
 // We then give the examples to be tested
