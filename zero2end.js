78675fe7748edc3eebc3e7e61c91999bbcc219d5
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-13 00:36:31 +0300

    draft

--- zero2end.js
+++ zero2end.js
@@ -2,7 +2,7 @@
 // We define the parameter we will initialize; we define a parameter that accepts our input of arrays
 // We then describe the expected return values; we will return the array with all other elements in place with the zeros pushed to the end
 // We create examples to use as tests; eg1, moveZeros([3,4,5,0,5,0,4,0]) // --> [3,4,5,5,4,0,0,0]  eg2, moveZeros([1,0,3,2,false,5,0,4]) // [1,3,2,false,5,4,0,0]  eg3 moveZeros([true,2,0,0,3,4,1]) // --> [true,2,3,4,1,0,0]
-// We then describe the code in non-technical terms using pseudo; we intake the array and loop through the array elements and if the element is equal to zero. we place it at the end of the array we are to return
+// We then describe the code in non-technical terms using pseudo; we intake the array and loop return the filtered array elements not equal to zero and then filter for elements equal to zero
 // We then give our solution code
 
 const moveZeros = function (arr) {
