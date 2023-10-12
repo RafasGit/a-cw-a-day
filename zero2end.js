f9ddcc044b6f5a879fa2183897f512b44179eb67
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-12 23:17:00 +0300

    draft

--- zero2end.js
+++ zero2end.js
@@ -1,7 +1,7 @@
 // We evaluate the problem we are to solve; we are to push all zeros in an array to the end
 // We define the parameter we will initialize; we define a parameter that accepts our input of arrays
 // We then describe the expected return values; we will return the array with all other elements in place with the zeros pushed to the end
-// We create examples to use as tests; eg1, moveZeros([3,4,5,0,5,0,4,0]) // --> moveZeros([3,4,5,5,4,0,0,0])  eg2, eg3
+// We create examples to use as tests; eg1, moveZeros([3,4,5,0,5,0,4,0]) // --> [3,4,5,5,4,0,0,0]  eg2, moveZeros([1,0,3,2,false,5,0,4]) // [1,3,2,false,5,4,0,0]  eg3
 // We then describe the code in non-technical terms using pseudo
 // We then give our solution code
 // We then test our examples
