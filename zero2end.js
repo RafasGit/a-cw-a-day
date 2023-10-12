7756a13b8529fda26c9b9500e9d63acbb0c3d08c
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-13 00:33:33 +0300

    draft

--- zero2end.js
+++ zero2end.js
@@ -4,15 +4,15 @@
 // We create examples to use as tests; eg1, moveZeros([3,4,5,0,5,0,4,0]) // --> [3,4,5,5,4,0,0,0]  eg2, moveZeros([1,0,3,2,false,5,0,4]) // [1,3,2,false,5,4,0,0]  eg3 moveZeros([true,2,0,0,3,4,1]) // --> [true,2,3,4,1,0,0]
 // We then describe the code in non-technical terms using pseudo; we intake the array and loop through the array elements and if the element is equal to zero. we place it at the end of the array we are to return
 // We then give our solution code
-// We then test our examples
 
-console.log(moveZeros([3,4,5,0,5,0,4,0]))
-console.log( moveZeros([1,0,3,2,false,5,0,4]))
-console.log(moveZeros([true,2,0,0,3,4,1]))
-
-var moveZeros = function (arr) {
+const moveZeros = function (arr) {
     return [
       ...(arr.filter(n => n !== 0)),
       ...(arr.filter(n => n === 0))
     ];
   }
+// We then test our examples
+
+console.log(moveZeros([3,4,5,0,5,0,4,0]))
+console.log( moveZeros([1,0,3,2,false,5,0,4]))
+console.log(moveZeros([true,2,0,0,3,4,1]))
