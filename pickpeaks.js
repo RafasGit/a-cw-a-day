ac4e95edf9b727beed8799a7075f41e7dc64b7cc
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-14 23:43:02 +0300

    prep

--- pickpeaks.js
+++ pickpeaks.js
@@ -2,4 +2,6 @@
 // We then define the parameters we are to use
 // We then declare the expected return value
 // We then give our example tests
+// We give our pseudocode
 // We test out our examples
+We 
