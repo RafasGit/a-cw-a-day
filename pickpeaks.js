ad81cf3110962d0421ca7d7c985a6450a1be431b
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-15 23:42:30 +0300

    prep

--- pickpeaks.js
+++ pickpeaks.js
@@ -1,4 +1,4 @@
-// We define the problem to solve; we are to find the maxima
+// We define the problem to solve; we are to find the maxima(peaks) of numbers
 // We then define the parameters we are to use
 // We then declare the expected return value
 // We then give our example tests
