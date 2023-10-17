df1946dda0e05119409985ae85673b7e3dbb1fa5
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-18 00:18:49 +0300

    returns

--- pickpeaks.js
+++ pickpeaks.js
@@ -1,6 +1,6 @@
 // We define the problem to solve; we are to find the maxima(peaks) of numbers in the array and as well indicate position of the peaks 
 // We then define the parameters we are to use; we will define an array to accept our array of integers, an object with the property of peaks to accept our peak values and the property of pos to indicate the position of our peaks. We also set run and peak as false booleans for our loop
-// We then declare the expected return value th
+// We then declare the expected return value; we will return the values that are peaks and the positions in which the elements are found
 // We then give our example tests
 // We give our pseudocode
 // We give our solution code
