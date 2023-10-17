88ba39253b8d732448b8cf039d284b5b1c54998b
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-18 00:22:37 +0300

    examples

--- pickpeaks.js
+++ pickpeaks.js
@@ -1,7 +1,7 @@
 // We define the problem to solve; we are to find the maxima(peaks) of numbers in the array and as well indicate position of the peaks 
 // We then define the parameters we are to use; we will define an array to accept our array of integers, an object with the property of peaks to accept our peak values and the property of pos to indicate the position of our peaks. We also set run and peak as false booleans for our loop
 // We then declare the expected return value; we will return the values that are peaks and the positions in which the elements are found
-// We then give our example tests
+// We then give our example tests eg; pickPeaks([1,1,1,1]) // --> {pos:[],peaks:[]} eg2; pickPeaks([1,2,2,3,2]) // --> {pos:[3], peaks:[3]} eg3; p
 // We give our pseudocode
 // We give our solution code
 // We test out our examples
