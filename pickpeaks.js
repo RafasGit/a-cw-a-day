c7add9758c99fd8b5ec3aaf744c2044a3b5f47b0
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-17 23:58:14 +0300

    parameters

--- pickpeaks.js
+++ pickpeaks.js
@@ -1,6 +1,6 @@
 // We define the problem to solve; we are to find the maxima(peaks) of numbers in the array and as well indicate position of the peaks 
 // We then define the parameters we are to use; we will define an ar to accept our array to accept our peak values
-// We then declare the expected return value
+// We then declare the expected return value th
 // We then give our example tests
 // We give our pseudocode
 // We give our solution code
