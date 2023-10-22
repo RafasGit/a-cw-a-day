81bc18c8899058f87c6b9ea4998f81dd9ff4d56f
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-23 00:33:12 +0300

    prep

--- tictaccheck.js
+++ tictaccheck.js
@@ -3,5 +3,5 @@
 // We then state the return value 
 // We then give examples to be used as tests; eg1; eg2; eg3;
 // We then give the non-technical explanation of our solution
-// We then give the code solution
+// We then give our code solution
 // We then test out our examples
