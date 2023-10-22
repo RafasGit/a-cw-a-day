bc406f1b4354af4f4d75b4a05d25cc3d28404fe4
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-10-23 00:28:07 +0300

    prep

--- tictaccheck.js
+++ tictaccheck.js
@@ -1,3 +1,4 @@
 // We will determine the issue to be solved
 // We define the parameters to be declared and the variables they accept
 // We then state the return value 
+// We then give examples to be used as tests
