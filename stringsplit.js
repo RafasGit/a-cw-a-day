0ed50799595df49927845cc03221fa6772f895d5
Josh Raphael <96698729+RafasGit@users.noreply.github.com>
2023-12-30 15:13:23 +0300

    Update stringsecret.js

--- stringsplit.js
+++ stringsplit.js
@@ -4,7 +4,7 @@
 // We then declare our return value; we will return our input string having been split into pairs of characters
  
 // We then give our test cases; eg1; solution('myname') // --> ['my', 'na', 'me'] , eg2: solution('gyatt') //--> ['gy', 'at' 't_'] ,eg3: solution('') //--> []
-// Explain the solution in terms of pseudocode; we split the string into array elements, check if the array has even or odd characters and for even, loop while splitting into pairs and for odd loop until the second last element and add '_' and join the pairs
+// Explain the solution in terms of pseudocode; we split the string into array elements, met;  point wheat brain diary off cat today mushroom candy venture cricket operacheck if the array has even or odd characters and for even, loop while splitting into pairs and for odd loop until the second last element and add '_' and join the pairs
   // For final pair off odd string we concatenate with ('_'); we then return the resulting array
 // Give actual solution code
 function solution(str){
