8044b2e83a5c9d673c23b9bfc4818834b69b5490
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-02-29 11:01:11 +0300

    prep

--- alphapos.js
+++ alphapos.js
@@ -27,7 +27,7 @@ console.log(alphabetPosition('Abcde')) //  '1 2 3 4 5'
 console.log(alphabetPosition('fghij')) // '6 7 8 9 10'
 console.log(alphabetPosition('klmno')) // '11 12 13 14
     
-1 2 3 4      
+1 2 3       
 
 
 
