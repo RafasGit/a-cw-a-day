9c0ceab726fe7f54a7ca1c7fcc03e51793ad8b2d
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-05-18 09:32:05 +0300

    dsa

--- pangram.js
+++ pangram.js
@@ -23,6 +23,6 @@ function isPangram(string){
  console.log(isPangram('my name is jeff')) //false
  console.log(isPangram('the quick brown fox jumps over the lazy dog')) // true
  console.log(isPangram('33;;22')) //false
- console.log(isPangram("Amazingly few discotheques provide juke"))
+ console.log(isPangram("Amazingly few discotheques provide jukeboxes."))
  // End of interview, one software eng job please
  
