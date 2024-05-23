80b8ad01a8c823ba1ddbcc12bde6fce067891b97
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-05-23 07:35:43 +0300

    dsa

--- tower.js
+++ tower.js
@@ -22,4 +22,4 @@ console.log(towerBuilder(2))  // ["*","***"]
 
 console.log(towerBuilder(4))  //["*","***","*****","*******"] 
 
-console.log(towerBuilder(5)) //["*","***"]
+console.log(towerBuilder(5)) //["*","***","*****"]
