633c0522276ce9becc3a3f68305993df8ae76a94
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-05-27 09:58:17 +0300

    dsa

--- digitalroot.js
+++ digitalroot.js
@@ -18,5 +18,5 @@ function digitalRoot(n) {
 // We test out our example test cases;
 console.log(digitalRoot(256))//13
 console.log(digitalRoot(304))//7
-console.log(digitalRoot(40))//
+console.log(digitalRoot(40))//4
 console.log(digitalRoot(456))
