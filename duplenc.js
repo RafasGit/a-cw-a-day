1cb6365fcfba58ab11f48ab8ff9e89edba8e2995
Josh Raphael <96698729+RafasGit@users.noreply.github.com>
2024-06-04 08:02:21 +0300

    prep

--- duplenc.js
+++ duplenc.js
@@ -1,4 +1,4 @@
-// We are to write a function that converts characters in a string into new string either '(' if converted string appears once or ')' if string appears twice
+ // We are to write a function that converts characters in a string into new string; either '(' if converted string appears once or ')' if string appears twice
 // We declare a single parameter that will take in input string, no integer or empty string inputs are expected, case should be ignored
 // We return the string '(' for characters that appear once and ')' for characters that are repeated
 //  Test cases; duplicateEncode('tommmy') --> '(())(', duplicateEncode('Musa') --> '(((('; duplicateEncode('exceed') --> ')(())('
