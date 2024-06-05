c570d6d0fb4a76b8bd2ca50d7492d8a2e43d6a10
Josh Raphael <96698729+RafasGit@users.noreply.github.com>
2024-06-05 16:39:07 +0300

    dsa

--- countdupl.js
+++ countdupl.js
@@ -20,4 +20,4 @@ function duplicateCount(text){
   console.log(duplicateCount("abcde")) // 0
   console.log(duplicateCount("aabbcde")) // 2
   console.log(duplicateCount("abbcde")) // 1
-  console.log(duplicateCount(""))
+  console.log(duplicateCount("bgfhgi")) 
