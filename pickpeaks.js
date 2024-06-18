abb641b85216429368019c51f5a63e27e2d979c3
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-06-18 07:19:01 +0300

    initialize

--- pickpeaks.js
+++ pickpeaks.js
@@ -33,7 +33,7 @@ function pickPeaks(arr)
    }
    return obj;
 }
-//We test out our examples
+//We test out our examples;
 console.log(pickPeaks([1,1,1,1])) // --> {pos:[],peaks:[]}
 console.log(pickPeaks([1,2,2,3,2])) // --> {pos:[3], peaks:[3]}
 console.log(pickPeaks([]))  // --> {pos:[] peaks:[]}
